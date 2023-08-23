d7aec8aa10a604d83b76c2f2c9c9fe934ca52c30
cheiy <gschege@gmail.com>
2023-08-23 17:37:44 +0000

    More annotations

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,7 +10,7 @@ class BasicCache(BaseCaching):
     """
     This class inherits from BaseCaching and is a caching system
     """
-    def put(self, key, item) -> None:
+    def put(self, key: str, item: str) -> None:
         """
         function assigns the item value to the dictionary cache_data for
         the key called key
@@ -18,7 +18,7 @@ class BasicCache(BaseCaching):
         if key is not None and item is not None:
             self.cache_data.update({key: item})
 
-    def get(self, key) -> Dict[str, str]:
+    def get(self, key: str) -> Dict[str, str]:
         """
         Function returns the value in self.cache_data linked to key
         """
